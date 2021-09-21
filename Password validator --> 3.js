0c54bc6aebbc7d16bf1dc4ac95da841ac3ead8a8
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-09-21 16:22:23 +0800

    Create Password validator --> 3.js

---
+++ Password validator --> 3.js
@@ -0,0 +1,27 @@
+/* You need to write regex that will validate a password to make sure it meets the following criteria:
+  1. At least six characters long
+  2. contains a lowercase letter
+  3. contains an uppercase letter
+  4. contains a number
+*/
+
+function validate(password) {
+  return /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(password)
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(validate('djI38D55'), true);
+    assert.strictEqual(validate('a2.d412'), false);
+    assert.strictEqual(validate('JHD5FJ53'), false);
+    assert.strictEqual(validate('!fdjn345'), false);
+    assert.strictEqual(validate('jfkdfj3j'), false);
+    assert.strictEqual(validate('123'), false);
+    assert.strictEqual(validate('abc'), false);
+    assert.strictEqual(validate('Password123'), true);
+  })
+})
