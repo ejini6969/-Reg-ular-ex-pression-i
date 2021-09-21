6dfb38eeca7c278cd64b5f231cac3f4b125a2bc1
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-09-21 16:40:20 +0800

    Create letter.js

---
+++ letter.js
@@ -0,0 +1,21 @@
+// Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
+
+String.prototype.isLetter = function() {
+  return /^[a-z]$/i.test(this)
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual("".isLetter(), false);
+    assert.strictEqual("a".isLetter(), true);
+    assert.strictEqual("X".isLetter(), true);
+    assert.strictEqual("7".isLetter(), false);
+    assert.strictEqual("*".isLetter(), false);
+    assert.strictEqual("ab".isLetter(), false);
+    assert.strictEqual("a\n".isLetter(), false);
+  });
+});
