78010a69586483147b218892ad4a0704a306a405
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-09-21 17:09:28 +0800

    Create PIN CODE --> 2.js

---
+++ PIN CODE --> 2.js
@@ -0,0 +1,21 @@
+/* Write a function that takes in a numeric code of any length. 
+  The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+*/
+
+function validateCode (code) {
+  return /^[1-3]/.test(code)
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(validateCode(123), true);
+    assert.strictEqual(validateCode(248), true);
+    assert.strictEqual(validateCode(8), false);
+    assert.strictEqual(validateCode(321), true);
+    assert.strictEqual(validateCode(9453), false);  
+  })
+})
