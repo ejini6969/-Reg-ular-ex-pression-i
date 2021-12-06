93a99fa6afe5a3acafc1270524c0ea6a2fda84a2
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-06 19:35:02 +0800

    Update letter.js

--- javascript/letter.js
+++ javascript/letter.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/567de72e8b3621b3c300000b
+
 // Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
 
 String.prototype.isLetter = function() {
