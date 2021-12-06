c3f807baf27e0d725861d3f2d35f4a6cb658ab1e
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-06 19:32:32 +0800

    Update digit.js

--- javascript/digit.js
+++ javascript/digit.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/567bf4f7ee34510f69000032
+
 // Implement a function which should return true if given object is a digit (0-9), false otherwise.
 
 String.prototype.digit = function() {
